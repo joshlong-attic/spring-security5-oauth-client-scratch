dda0d660b6a770d012269d8557a684efa30452d6
Josh Long <josh@joshlong.com>
2017-08-26 18:03:53 +0800

    scratch

---
+++ src/main/java/com/example/oauthsecurity5client/OauthSecurity5ClientApplication.java
@@ -0,0 +1,12 @@
+package com.example.oauthsecurity5client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthSecurity5ClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthSecurity5ClientApplication.class, args);
+	}
+}
